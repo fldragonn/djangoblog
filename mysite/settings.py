4c2304a5ffa662b675b90d7a31904b33c05daa4b
FLDRAGONJUN <fldragonjun@gmail.com>
2019-06-09 22:56:23 +0900

    HOST bug fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l*^7p1rw&=syp8nmoe58k268fr-_#u5p#ltq5rs-zs(48r_-0c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
