7c89c227183a52e846aea0562a223f36885721b0
fldragonjun@gmail.com <fldragonjun@gmail.com>
2019-06-14 17:23:30 +0900

    vscode bug fix

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    objects = models.Manager()
     author =  models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
